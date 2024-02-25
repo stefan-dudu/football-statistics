21aa9bceb29f615fea890cf25b5fd63623ab1709
Stefan Dudu <stefanut-george.dudu-extern@deutschebahn.com>
2024-02-25 11:53:39 +0200

    update api key & secret

--- src/api/api-sports.js
+++ src/api/api-sports.js
@@ -3,7 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://v3.football.api-sports.io",
   headers: {
-    "x-apisports-key": "13e62d9b05d6a2943e4605b902033228",
-    // "x-apisports-key": "e556890211869fc7dd70cff32efb8e09",
+    "x-apisports-key": process.env.API_KEY,
   },
 });
